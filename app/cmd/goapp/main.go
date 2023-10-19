d53580a6ffcce57e715ce8a510cedbaed8170300
Evgeny Kirichuk <zhenyakirichuk@gmail.com>
2023-10-19 19:18:26 +0200

    add a server boilerplate

--- app/cmd/goapp/main.go
+++ app/cmd/goapp/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"flag"
 	"goapp/internal/log"
 	"goapp/internal/scylla"
 
 	"github.com/gocql/gocql"
+	"github.com/gofiber/fiber/v2"
 	"go.uber.org/zap"
 )
 
+var config = fiber.Config{
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+}
+
+
 func main() {
+	addr := flag.String("addr", ":3000", "http service address")
+	flag.Parse()
+
 	logger := log.CreateLogger("info")
 
 	cluster := scylla.CreateCluster(gocql.Quorum, "catalog", "scylla-node1", "scylla-node2", "scylla-node3")
@@ -18,11 +30,19 @@ func main() {
 	}
 	defer session.Close()
 
-	scylla.SelectQuery(session, logger)
-	insertQuery(session, logger)
-	scylla.SelectQuery(session, logger)
-	deleteQuery(session, logger)
-	scylla.SelectQuery(session, logger)
+	app := fiber.New(config)
+	apiv1 := app.Group("/api/v1")
+	apiv1.Get("/user", func(c *fiber.Ctx) error {
+		return c.JSON(map[string]string{"message": "Hello World"})
+	})
+
+	app.Listen(*addr)
+
+	// scylla.SelectQuery(session, logger)
+	// insertQuery(session, logger)
+	// scylla.SelectQuery(session, logger)
+	// deleteQuery(session, logger)
+	// scylla.SelectQuery(session, logger)
 }
 
 func insertQuery(session *gocql.Session, logger *zap.Logger) {
--- app/go.mod
+++ app/go.mod
@@ -5,7 +5,7 @@ go 1.13
 require (
 	github.com/aybabtme/rgbterm v0.0.0-20170906152045-cc83f3b3ce59 // indirect
 	github.com/gocql/gocql v0.0.0-20191106222750-ae2f7fc85f32
-	github.com/mattn/go-isatty v0.0.10 // indirect
+	github.com/gofiber/fiber/v2 v2.50.0
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/qeesung/image2ascii v1.0.1
 	github.com/scylladb/gocqlx v1.3.1
